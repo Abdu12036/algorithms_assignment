3cae048f974b116737ad0b8eac9f431657523820
Abdunur <amangeldievabdunur@gmail.com>
2025-03-29 23:15:41 +0600

    added fibonacci function

--- Main.java
+++ Main.java
@@ -41,6 +41,6 @@ public class Main {
 
     public static void main(String[] args) {
         Recursion recursion = new Recursion();
-        System.out.println(recursion.factorial(5));
+        System.out.println(recursion.fibonacci(8));
     }
 }
--- recursion/Recursion.java
+++ recursion/Recursion.java
@@ -10,4 +10,15 @@ public class Recursion {
         // untill n becomes 1
         else return n*factorial(n-1);
     }
+
+    // function for finding the n-th term of fibonacci sequence
+    // it uses a recursive approach
+    public int fibonacci(int n) {
+        // the first term is 0 and second term is 1
+        if(n==1)return 0;
+        else if(n==2)return 1;
+        // sums up the previous two terms of the sequence
+        else return fibonacci(n-1) + fibonacci(n-2);
+    }
+
 }
